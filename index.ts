fda7fbcf13e0d8670461f902cadc7a42796135cb
yosiaLukumai <yosialukumai@gmail.com>
2025-11-13 18:48:42 +0300

    all the files need

---
+++ index.ts
@@ -0,0 +1,54 @@
+import express from 'express';
+import { Ollama } from 'ollama';
+const app = express();
+const port = 3000;
+const OLLAMA_API_KEY = "f2cbadd69d764dadb91891a7e8c49b69.iH52kXPTW4zGrDUltoM4b50y";
+
+const ollama = new Ollama({
+    host: 'https://ollama.com',
+    headers: { Authorization: 'Bearer ' + OLLAMA_API_KEY }
+})
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Forward Server is running!');
+});
+
+
+app.post("/api/ai-insight", async (req: any, res: any) => {
+    try {
+        const { prompt } = req.body;
+        const response = await ollama.chat({
+            model: 'deepseek-v3.1:671b-cloud',
+            messages: [{
+                role: 'user',
+                content: prompt
+            }]
+        });
+        return res.json({
+            success: true,
+            response: response?.message?.content
+        })
+    } catch (error) {
+        return res.json({
+            success: false,
+            response: null
+        })
+    }
+})
+
+
+app.post("api/trend/analysis", (req: any, res: any) => {
+    try {
+
+    } catch (error) {
+
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server listening at at ${port}`)
+});
+
